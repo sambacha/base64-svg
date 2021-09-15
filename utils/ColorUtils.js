f7b52ba74f61bf02ce2799c4b1264bb011c3c5a5
sam bacha <sam@manifoldfinance.com>
2021-09-15 03:24:05 -0700

    prettier debug issue

--- utils/ColorUtils.js
+++ utils/ColorUtils.js
@@ -1,18 +1,18 @@
 var ColorUtils = (() => {
-  var emptyColor = '#000000'
-  
-  var _intToPaddedHex = int => ('00' + int.toString(16)).slice(-2)
-  
-  var _randomChannel = () => Math.floor(Math.random() * 256)
+  var emptyColor = '#000000';
+
+  var _intToPaddedHex = (int) => ('00' + int.toString(16)).slice(-2);
+
+  var _randomChannel = () => Math.floor(Math.random() * 256);
 
   var randomColor = () => ({
     r: _randomChannel(),
     g: _randomChannel(),
     b: _randomChannel(),
-    a: 1
-  })
+    a: 1,
+  });
 
-  var priceColorMaxPrice = 100000000000000
+  var priceColorMaxPrice = 100000000000000;
 
   /*
     i = 0   => 0, 255, 0 green
@@ -20,36 +20,52 @@ var ColorUtils = (() => {
     i = 1   => 255, 0, 0 red
     clamped at 0.0001 eth for now...
   */
-  var priceAsColor = price => {    
-    let intensity = price / priceColorMaxPrice
-    return new Uint8ClampedArray([510 * intensity, 510 * (1 - intensity), 0, 255])
-  }
+  var priceAsColor = (price) => {
+    let intensity = price / priceColorMaxPrice;
+    return new Uint8ClampedArray([
+      510 * intensity,
+      510 * (1 - intensity),
+      0,
+      255,
+    ]);
+  };
 
-  var rgbaToHex = rgba => `#${ _intToPaddedHex(rgba.r) }${ _intToPaddedHex(rgba.g) }${ _intToPaddedHex(rgba.b) }`
+  var rgbaToHex = (rgba) =>
+    `#${_intToPaddedHex(rgba.r)}${_intToPaddedHex(rgba.g)}${_intToPaddedHex(
+      rgba.b,
+    )}`;
 
-  var rgbaToBytes3 = rgba => `0x${ rgbaToHex(rgba).substr(1,6) }`
+  var rgbaToBytes3 = (rgba) => `0x${rgbaToHex(rgba).substr(1, 6)}`;
 
-  var rgbaToIntArray = rgba => [rgba.r, rgba.g, rgba.b, rgba.a * 255]
+  var rgbaToIntArray = (rgba) => [rgba.r, rgba.g, rgba.b, rgba.a * 255];
 
-  var rgbaToString = rgba => `rgba(${Object.values(rgba).join(', ')})`
+  var rgbaToString = (rgba) => `rgba(${Object.values(rgba).join(', ')})`;
 
-  var intArrayToRgba = int_array => ({
+  var intArrayToRgba = (int_array) => ({
     r: int_array[0],
     g: int_array[1],
     b: int_array[2],
-    a: int_array[3] / 255
-  })
+    a: int_array[3] / 255,
+  });
 
-  var intArrayToHex = int_array => `#${ _intToPaddedHex(int_array[0]) }${ _intToPaddedHex(int_array[1]) }${ _intToPaddedHex(int_array[2]) }`
-  
-  var bytes3ToRgba = bytes3 => ({
+  var intArrayToHex = (int_array) =>
+    `#${_intToPaddedHex(int_array[0])}${_intToPaddedHex(
+      int_array[1],
+    )}${_intToPaddedHex(int_array[2])}`;
+
+  var bytes3ToRgba = (bytes3) => ({
     r: parseInt(bytes3.substr(2, 2), 16),
     g: parseInt(bytes3.substr(4, 2), 16),
     b: parseInt(bytes3.substr(6, 2), 16),
-    a: 1
-  })
-  
-  var bytes3ToIntArray = bytes3 => [parseInt(bytes3.substr(2, 2), 16), parseInt(bytes3.substr(4, 2), 16), parseInt(bytes3.substr(6, 2), 16), 255]
+    a: 1,
+  });
+
+  var bytes3ToIntArray = (bytes3) => [
+    parseInt(bytes3.substr(2, 2), 16),
+    parseInt(bytes3.substr(4, 2), 16),
+    parseInt(bytes3.substr(6, 2), 16),
+    255,
+  ];
 
   return {
     rgbaToBytes3: rgbaToBytes3,
@@ -63,8 +79,8 @@ var ColorUtils = (() => {
     emptyColor: emptyColor,
     randomColor: randomColor,
     priceAsColor: priceAsColor,
-    priceColorMaxPrice: priceColorMaxPrice
-  }
-})()
+    priceColorMaxPrice: priceColorMaxPrice,
+  };
+})();
 
-export default ColorUtils
+export default ColorUtils;
