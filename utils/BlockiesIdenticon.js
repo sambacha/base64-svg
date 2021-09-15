55af071ad2308ac1135b637d6faf237f9c79ca8b
sam bacha <sam@manifoldfinance.com>
2021-09-15 03:25:06 -0700

    v1 (#1)

--- utils/BlockiesIdenticon.js
+++ utils/BlockiesIdenticon.js
@@ -1,22 +1,17 @@
-import React, {
-  PureComponent
-} from 'react'
-import {
-  toDataUrl
-} from './blockies/blockies'
-import './BlockiesIdenticon.css'
+import React, { PureComponent } from 'react';
+import { toDataUrl } from './blockies/blockies';
+import './BlockiesIdenticon.css';
 
 class BlockiesIdenticon extends PureComponent {
   render() {
-    return <img className = 'blockie'
-    src = {
-      toDataUrl(this.props.account)
-    }
-    alt = {
-      this.props.account
-    }
-    />
+    return (
+      <img
+        className="blockie"
+        src={toDataUrl(this.props.account)}
+        alt={this.props.account}
+      />
+    );
   }
 }
 
-module.exports = BlockiesIdenticon
+module.exports = BlockiesIdenticon;
