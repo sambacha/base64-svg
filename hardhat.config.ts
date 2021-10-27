62da34fe966d392905ecb279411a19568d4a84c0
sam bacha <sam@manifoldfinance.com>
2021-10-26 20:15:12 -0700

    build(hh): pragma versioning

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,18 @@ const mnemonic = 'test test test test test test test test test test test junk';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.0',
+    version: '^0.8.0',
+    settings: {
+      metadata: {
+        bytecodeHash: 'none',
+      },
+      optimizer: {
+        enabled: false,
+        runs: 200,
+        details: {
+          yul: false,
+        },
+    version: '^0.7.0',
     settings: {
       metadata: {
         bytecodeHash: 'none',
