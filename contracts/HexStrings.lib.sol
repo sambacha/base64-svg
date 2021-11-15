d45cdc8ac7dd25dc2648b87e0e9b635323c731c5
sam bacha <sam@manifoldfinance.com>
2021-11-15 13:00:54 -0800

    Create HexStrings.lib.sol

---
+++ contracts/HexStrings.lib.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0 || ^0.8.0;
+
+/// @title HexStrings
+library HexStrings {
+
+    bytes16 internal constant ALPHABET = "0123456789abcdef";
+
+    // @function toHexString
+    function toHexString(uint256 value, uint256 length)
+        internal
+        pure
+        returns (string memory)
+    {
+        bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = "0";
+        buffer[1] = "x";
+        for (uint256 i = 2 * length + 1; i > 1; --i) {
+            buffer[i] = ALPHABET[value & 0xf];
+            value >>= 4;
+        }
+        
+        return string(buffer);
+    }
+    
+}
